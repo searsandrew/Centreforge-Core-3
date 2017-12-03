627990c697f3808875a6fe9fe7a203bf1d3447a6
Andrew Sears <andrew@mayfifteenth.com>
2017-12-02 21:34:39 -0600

    Implement function features from twentyseventeen

--- functions.php
+++ functions.php
@@ -9,9 +9,31 @@ if(!function_exists( 'cf3_setup' ))
     function cf3_setup()
     {
         add_theme_support('automatic-feed-links');
+        add_theme_support('title-tag');
         add_theme_support('post-thumbnails');
         add_theme_support('custom-header');
         add_theme_support('custom-background');
+        add_theme_support('html5', array(
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+        ));
+        add_theme_support('post-formats', array(
+            'aside',
+            'image',
+            'video',
+            'quote',
+            'link',
+            'gallery',
+            'audio',
+        ));
+        add_theme_support('custom-logo', array(
+            'width'       => 250,
+            'height'      => 250,
+            'flex-width'  => true,
+        ));
+        add_theme_support('customize-selective-refresh-widgets');
     }
     add_action('after_setup_theme', 'cf3_setup');
 }
@@ -53,3 +75,43 @@ if(!function_exists( 'cf3_remove_welcome_panel' ))
     }
     add_action( 'load-index.php', 'cf3_remove_welcome_panel' );
 }
+
+/* Replaces "[...]" (appended to automatically generated excerpts) with ... and a 'Continue reading' link.
+ * since: centreforge 3.0.0
+ * modified: centreforge 3.0.0
+ */
+
+function cf3_excerpt_more( $link ) {
+	if ( is_admin() ) {
+		return $link;
+	}
+
+	$link = sprintf('<p class="link-more"><a href="%1$s" class="more-link">%2$s</a></p>',
+		esc_url(get_permalink(get_the_ID())),
+		/* translators: %s: Name of current post */
+		sprintf('Continue reading<span class="screen-reader-text"> "%s"</span>', get_the_title( get_the_ID()))
+	);
+	return ' &hellip; ' . $link;
+}
+add_filter( 'excerpt_more', 'cf3_excerpt_more' );
+
+/* Handles JavaScript detection. Adds a `js` class to the root `<html>` element when JavaScript is detected.
+ * since: centreforge 3.0.0
+ * modified: centreforge 3.0.0
+ */
+
+function cf3_javascript_detection() {
+	echo "<script>(function(html){html.className = html.className.replace(/\bno-js\b/,'js')})(document.documentElement);</script>\n";
+}
+add_action( 'wp_head', 'cf3_javascript_detection', 0 );
+
+/* Add a pingback url auto-discovery header for singularly identifiable articles.
+ * since: centreforge 3.0.0
+ * modified: centreforge 3.0.0
+ */
+function cf3_pingback_header() {
+	if ( is_singular() && pings_open() ) {
+		printf( '<link rel="pingback" href="%s">' . "\n", get_bloginfo( 'pingback_url' ) );
+	}
+}
+add_action( 'wp_head', 'cf3_pingback_header' );
